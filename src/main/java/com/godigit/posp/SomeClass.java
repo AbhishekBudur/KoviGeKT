b2477374f927e8981a03d41a9fd59956fc8d693d
AbhishekBudur <abhishekchotu0@gmail.com>
2025-02-24 20:52:24 +0530

    Second Commit

--- src/main/java/com/godigit/posp/SomeClass.java
+++ src/main/java/com/godigit/posp/SomeClass.java
@@ -4,7 +4,7 @@ public class SomeClass {
 
 	public static void main(String[] args) {
 		System.out.println("Some text to see the git");
-
+		System.out.println("Second commit line to test");
 	}
 
 }
